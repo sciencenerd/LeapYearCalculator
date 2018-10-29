44f0613edfb2c8386b6fea34a72cc9b0e8821f20
anwar <anwar.lynch@gmail.com>
2018-10-29 15:22:37 -0500

    first run of code

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,22 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println(isLeapYear(-1600)); //should return false (out of range)
+        System.out.println(isLeapYear(1600)); //should return true (leap year)
+        System.out.println(isLeapYear(2017)); //should return false (not leap year)
+        System.out.println(isLeapYear(2000)); //should return true (leap year)
 
+    }
+
+
+    public static boolean isLeapYear(int year){
+        if(year >= 1 && year <= 9999){
+            if((year % 4 == 0) && (year % 100 != 0)){
+                return true;
+            }else if(year % 400 == 0){
+                return true;
+            }else{return false;}
+
+        }else{return false;}
     }
 }
